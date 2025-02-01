3d53deae52aaf7b4e4eac5ce807445150c9f899f
asaiah4812 <hensonasaiah21@gmail.com>
2025-02-01 11:21:56 +0100

    hng

--- core/views.py
+++ core/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
-from rest_framework import generics
+from django.http import JsonResponse
 from .models import Dreamer
 from .serializer import DreamerSerializer
+from rest_framework.decorators import api_view
 
-# Create your views here.
-
-class DreamerList(generics.ListAPIView):
-    queryset = Dreamer.objects.all()
-    serializer_class = DreamerSerializer
+@api_view(['GET'])
+def dreamer_list(request):
+    if request.method == 'GET':
+        dreamer = Dreamer.objects.first()
+        data = {
+                "email": dreamer.email,
+                "current_datetime": dreamer.current_datetime,
+                "github_url": dreamer.github_url,
+            }
+        return JsonResponse(data, status=200, safe=False)
+    return JsonResponse({"error": "Method not allowed"}, status=405)
--- my_project_hng/settings.py
+++ my_project_hng/settings.py
@@ -32,7 +32,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['dreamerhngtask-production.up.railway.app', '127.0.0.1']
 
-CSRF_TRUSTED_ORIGINS = ['https://dreamerhngtask-production.up.railway.app']
+# CSRF_TRUSTED_ORIGINS = ['https://dreamerhngtask-production.up.railway.app', '127.0.0.1']
 
 
 # Application definition
@@ -70,9 +70,7 @@ CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will
 CORS_ALLOW_CREDENTIALS = True
 
 
-CORS_ALLOWED_ORIGINS = [
-    "dreamerhngtask-production.up.railway.app"
-]
+CSRF_TRUSTED_ORIGINS = ["https://dreamerhngtask-production.up.railway.app"]
 
 
 
